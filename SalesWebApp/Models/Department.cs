83138796d1dd64f12ea06bb711055c6de08a2c9c
bernardesvini <vberalves@gmail.com>
2023-07-03 21:00:37 +0100

    Other Entities and Second Migration

--- SalesWebApp/Models/Department.cs
+++ SalesWebApp/Models/Department.cs
@@ -9,5 +9,34 @@ namespace SalesWebApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            // SEM LAMBDA
+
+            //double totalSales = 0.0;
+
+            //foreach (Seller seller in Sellers)
+            //    totalSales += seller.TotalSales(initial, final);
+            
+            //return totalSales;
+
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
