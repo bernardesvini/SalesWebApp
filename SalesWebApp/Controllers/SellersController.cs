db51ba0bfd536b7302ff15d09292560f8416c0b2
bernardesvini <vberalves@gmail.com>
2023-07-04 21:13:41 +0100

    Created Form

--- SalesWebApp/Controllers/SellersController.cs
+++ SalesWebApp/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebApp.Services;
+using SalesWebApp.Models;
 
 namespace SalesWebApp.Controllers
 {
@@ -21,9 +22,22 @@ namespace SalesWebApp.Controllers
 
             var list = _sellerService.FindAll();
             return View(list);
-           
+
             // Mycomment - abaixo como pensei
-           // return View(_sellerService.FindAll());
+            // return View(_sellerService.FindAll());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
